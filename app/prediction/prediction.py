573bb8c2a9f6e39cfa578380a442739aaca995ac
LisourSane123 <l.swietochow@student.uw.edu..pl>
2026-08-20 18:21:00 +0200

    poczatek predykcji

--- app/prediction/prediction.py
+++ app/prediction/prediction.py
@@ -3,8 +3,10 @@ from datetime import date, datetime, timezone
 from pathlib import Path
 import polars as pl
 import sqlalchemy as sa
+from pathlib import Path
 
-engine = sa.create_engine("sqlite:///file:/home/pi/Waga_RP/data/waga.db?mode=ro&uri=true")
+DB = Path(__file__).resolve().parents[2] / "data" / "waga.db"
+engine = sa.create_engine(f"sqlite:///file:{DB}?mode=ro&uri=true")
 with engine.connect() as conn:
     query = sa.text("SELECT * FROM users")
     df = pl.read_database(query, conn)
