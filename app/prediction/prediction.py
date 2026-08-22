befce487de7d85a915177a4262c8d0c1c7698fda
LisourSane123 <l.swietochow@student.uw.edu..pl>
2026-08-22 12:34:19 +0200

    test and bugfix

--- app/prediction/prediction.py
+++ app/prediction/prediction.py
@@ -10,11 +10,11 @@ engine = sa.create_engine(f"sqlite:///file:{DB}?mode=ro&uri=true")
 with engine.connect() as conn:
     query_garmin_daily = sa.text("""SELECT start_time_local, distance, moving_time_s, total_elevation_gain, average_speed, max_speed, average_heart_rate, max_heart_rate, 
                                     calories, aerobic_te, anaerobic_te 
-                                    FROM garmin_daily
+                                    FROM garmin_activities
                                     WHERE sport_type = 'running'""")
     query_garmin_activities = sa.text("""SELECT day, steps, floors_climbed, calories_total, calories_active, resting_hr, avg_stress, body_battery_high, sleep_seconds, 
                                          deep_sleep_seconds, light_sleep_seconds, rem_sleep_seconds, awake_seconds, sleep_score, hrv_status, treaning_readiness 
-                                         FROM garmin_activities""")
+                                         FROM garmin_daily""")
     query_scale_mesurements = sa.text("""SELECT recorded_at, weight_kg, fat_percentage, muscle_mass_kg, bone_mass_kg, water_percentage
                                          from measurements""")
     garmin_daily_df = pl.read_database(query_garmin_daily, conn)
